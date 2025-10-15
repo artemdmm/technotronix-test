2815f192dba598cf1f422441224298410fd53872
artemdmm <145367476+artemdmm@users.noreply.github.com>
2025-10-15 23:00:22 +0500

    8080 -> 8000

--- src/main.py
+++ src/main.py
@@ -30,4 +30,4 @@ app.include_router(device_router, prefix="/devices")
 app.include_router(accumulator_router, prefix="/accumulators")
 
 if __name__  == "__main__": 
-    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
